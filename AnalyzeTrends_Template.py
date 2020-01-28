2c5e7236edd0088730d97680e3627213324050e6
Made Lapuerta <mlapuerta@college.harvard.edu>
2020-01-27 23:06:36 -0500

    adding AnalyzeTrends file

---
+++ AnalyzeTrends_Template.py
@@ -0,0 +1,149 @@
+#### CHANEL ####
+chanel_collection_name = "CHANEL SS20 READY TO WEAR"
+
+## DEFINE THE TRENDS
+chanel_trends = {
+'plaid':0, 
+'stripes':0, 
+'ankle_strap':0, 
+'open_toe':0, 
+'blazer':0, 
+'v_neck':0, 
+'hat':0,
+'costume_jewelry':0, 
+'strapless':0, 
+'button_down':0, 
+'hot_shorts':0, 
+'leather':0, 
+'maxi':0,
+'crossbody_bag':0, 
+'clutch':0, 
+'cc_logo':0, 
+'chanel_logo':0, 
+'red':0, 
+'black':0, 
+'blue':0, 
+'yellow':0, 
+'pink':0, 
+'orange':0, 
+'white':0, 
+'navy':0, 
+'green':0, 
+'shimmer':0, 
+'grey':0
+}
+
+## DEFINE THE OUTFITS
+chanel_looks = [
+['plaid', 'blazer', 'v_neck', 'ankle_strap', 'red'], # LOOK 1
+['hat', 'costume_jewelry', 'blazer', 'v_neck', 'tights', 'open_toe', 'black', 'clutch'],
+['plaid', 'costume_jewelry', 'blazer', 'v_neck', 'tights', 'red', 'ankle_strap'],
+['plaid', 'red', 'open_toe', 'ankle_strap', 'strapless'],
+['plaid', 'red', 'open_toe', 'ankle_strap', 'costume_jewelry', 'button_down'],
+['plaid', 'blazer', 'red', 'black', 'stripes', 'ankle_strap'],
+['plaid', 'blazer', 'costume_jewelry', 'ankle_strap', 'red', 'white', 'v_neck'],
+['plaid', 'black', 'costume_jewelry', 'button_down', 'ankle_strap'],
+['plaid', 'pink', 'blue', 'ankle_strap', 'blazer', 'costume_jewelry', 'cossbody_bag'],
+['plaid', 'pink', 'open_toe', 'tights', 'costume_jewelry', 'blazer', 'cc_logo'],
+['plaid', 'button_down', 'ankle_strap', 'pink', 'blue', 'orange', 'clutch'],
+['plaid', 'black', 'pink', 'clutch', 'ankle_strap', 'blazer', 'chanel_logo'],
+['plaid', 'black', 'ankle_strap', 'clutch'],
+['plaid', 'blazer', 'black', 'blue', 'orange', 'pink', 'ankle_strap', 'button_down', 'costume_jewelry'],
+['plaid', 'pink', 'blue', 'open_toe', 'ankle_strap'],
+['blazer', 'costume_jewelry', 'pink', 'blue', 'black', 'open_toe', 'ankle_strap', 'clutch'],
+['sweater', 'stripes', 'pink', 'black', 'ankle_strap', 'open_toe'],
+['blazer', 'chanel_logo', 'tights', 'ankle_strap', 'open_toe', 'v_neck', 'costume_jewelry', 'hat', 'black', 'white'],
+['stripes', 'orange', 'black', 'tights', 'ankle_strap', 'open_toe', 'button_down'],
+['cc_logo', 'crossbody_bag', 'tights', 'ankle_strap', 'open_toe', 'black', 'orange'],
+['stripes', 'button_down', 'tights', 'ankle_strap', 'open_toe', 'blazer', 'hot_shorts', 'clutch'],
+['clutch', 'navy', 'ankle_strap', 'open_toe', 'costume_jewelry', 'cc_logo'],
+['blazer', 'stripes', 'white', 'ankle_strap', 'clutch', 'green'],
+['blue', 'green', 'clutch', 'ankle_strap', 'open_toe', 'white', 'cc_logo', 'tights'],
+['white', 'red', 'tights', 'plaid', 'cc_logo', 'navy', 'crossbody_bag', 'ankle_strap', 'open_toe'],
+['white', 'black', 'costume_jewelry', 'open_toe', 'ankle_strap', 'tights'],
+['red', 'black', 'tights', 'cc_logo', 'crossbody_bag', 'leather', 'open_toe', 'ankle_strap'],
+['red', 'black', 'leather', 'cc_logo', 'tights', 'open_toe', 'ankle_strap'],
+['red', 'shimmer', 'ankle_strap', 'blazer'],
+['red', 'black', 'open_toe', 'ankle_strap', 'costume_jewelry', 'tights', 'crossbody_bag', 'stripes'],
+['red', 'black', 'open_toe', 'ankle_strap', 'maxi'],
+['red', 'black', 'costume_jewelry', 'cc_logo', 'maxi', 'button_down'],
+['red', 'black', 'maxi', 'cc_logo', 'tights', 'ankle_strap'],
+['red', 'stripes', 'cc_logo', 'blazer', 'open_toe', 'ankle_strap'],
+['blazer', 'red', 'stripes', 'clutch', 'chanel_logo', 'ankle_strap', 'cc_logo', 'costume_jewelry', 'orange'],
+['white', 'red', 'blazer', 'cc_logo', 'stripes', 'ankle_strap', 'open_toe'],
+['blazer', 'button_down', 'red', 'white', 'navy', 'ankle_strap', 'cc_logo'],
+['white', 'black', 'tights', 'crossbody_bag', 'ankle_strap', 'v_neck', 'cc_logo'],
+['navy', 'white', 'red', 'ankle_strap', 'open_toe', 'costume_jewelry', 'clutch'],
+['navy', 'white', 'stripes', 'costume_jewelry', 'ankle_strap'],
+['hot_shorts', 'cc_logo', 'ankle_strap', 'open_toe', 'pink', 'yellow', 'costume_jewelry'],
+['grey', 'clutch', 'blazer', 'button_down', 'hot_shorts', 'open_toe', 'ankle_strap', 'shimmer'],
+['hot_shorts', 'shimmer', 'grey', 'blazer', 'open_toe', 'ankle_strap', 'crossbody_bag'],
+['cc_logo', 'costume_jewelry', 'clutch', 'ankle_strap', 'open_toe', 'grey', 'shimmer'],
+['grey', 'crossbody_bag', 'ankle_strap', 'button_down', 'cc_logo', 'red'],
+['white', 'hot_shorts', 'ankle_strap', 'button_down', 'cc_logo', 'costume_jewelry'],
+['pink', 'ankle_strap', 'costume_jewelry', 'stripes', 'cc_logo'],
+['stripes', 'pink', 'costume_jewelry', 'clutch', 'tights', 'ankle_strap', 'blazer'],
+['cc_logo', 'pink', 'shimmer', 'ankle_strap', 'open_toe', 'hot_shorts', 'costume_jewelry'],
+['pink', 'black', 'tights', 'ankle_strap', 'open_toe', 'hot_shorts', 'shimmer', 'cc_logo', 'costume_jewelry'],
+['white','cc_logo', 'maxi', 'open_toe', 'pink', 'costume_jewelry'],
+['black', 'tights', 'pink', 'costume_jewelry', 'ankle_strap', 'open_toe'],
+['cc_logo', 'black', 'tights', 'open_toe', 'ankle_strap'],
+['maxi', 'navy', 'open_toe', 'ankle_strap', 'costume_jewelry', 'cc_logo'],
+['navy', 'tights', 'chanel_logo', 'cc_logo', 'costume_jewelry', 'ankle_strap', 'open_toe'],
+['white', 'navy', 'black', 'chanel_logo', 'clutch', 'costume_jewelry', 'open_toe', 'ankle_strap', 'maxi'],
+['maxi', 'white', 'navy', 'black', 'chanel_logo','clutch', 'costume_jewelry', 'open_toe', 'ankle_strap'],
+['open_toe', 'ankle_strap', 'blazer', 'button_down', 'white', 'navy', 'black', 'cc_logo', 'maxi'],
+['button_down', 'costume_jewelry', 'chanel_logo', 'ankle_strap', 'white', 'navy', 'black', 'maxi'],
+['cc_logo', 'button_down', 'open_toe', 'ankle_strap', 'white', 'black', 'navy', 'maxi'],
+['black', 'white', 'tights', 'blue', 'open_toe', 'ankle_strap', 'cc_logo'],
+['black', 'white', 'cc_logo', 'tights', 'ankle_strap', 'costume_jewelry'],
+['black', 'tights', 'v_neck', 'crossbody_bag', 'ankle_strap', 'open_toe', 'cc_logo'],
+['hot_shorts', 'tights', 'ankle_strap', 'open_toe', 'cc_logo', 'black', 'costume_jewelry'],
+['black', 'blazer', 'clutch', 'costume_jewelry', 'chanel_logo', 'open_toe', 'ankle_strap'],
+['black', 'cc_logo', 'open_toe', 'ankle_strap', 'clutch'],
+['black', 'navy', 'tights', 'open_toe', 'ankle_strap', 'clutch', 'shimmer', 'cc_logo', 'hot_shorts'],
+['white', 'black', 'cc_logo', 'tights', 'open_toe', 'ankle_strap', 'costume_jewelrry', 'shimmer', 'hot_shorts'],
+['cc_logo', 'pink', 'black', 'tights', 'shimmer', 'crossbody_bag', 'ankle_strap', 'open_toe', 'hot_shorts'],
+['white', 'black', 'pink', 'ankle_strap'],
+['black', 'pink', 'blue', 'ankle_strap', 'button_down'],
+['black', 'tights', 'open_toe', 'ankle_strap', 'button_down', 'chanel_logo'],
+['black', 'v_neck', 'tights', 'open_toe', 'ankle_strap', 'cc_logo'],
+['white', 'black', 'red', 'open_toe', 'ankle_strap', 'hat'],
+['cc_logo', 'costume_jewelry', 'tights', 'open_toe', 'ankle_strap', 'white', 'black'],
+['hat', 'white', 'black', 'tights', 'open_toe', 'ankle_strap', 'costume_jewelry'],
+['cc_logo', 'tights', 'open_toe', 'ankle_strap', 'v_neck', 'costume_jewelry', 'white', 'black'],
+['white', 'black', 'red', 'tights', 'open_toe', 'ankle_strap', 'hat'],
+['black', 'tights', 'ankle-strap', 'open_toe'],
+['black', 'white', 'maxi', 'button_down', 'open_toe', 'ankle_strap'],
+['black', 'white', 'cc_logo', 'clutch', 'maxi', 'open_toe', 'ankle_strap'],
+['black', 'cc_logo', 'open_toe', 'ankle_strap', 'maxi'],
+['cc_logo', 'black', 'v_neck', 'open_toe', 'maxi'],
+]
+
+
+## TODO: UPDATE THIS WITH THE LOOKS YOU WANT TO ANALYZE:
+collection_name = chanel_collection_name
+looks = chanel_looks
+total = len(looks)
+trends = chanel_trends
+
+## PRINT INFO:
+print("ANALYZING TRENDS FROM", collection_name)
+
+## GET TREND DATA (count)
+for trend, var in trends.items():
+    count = 0
+    for look in looks:
+        if str(trend) in look:
+            count += 1
+    trends.update({trend:count})
+
+## GET PERCENTAGES
+trend_percentages = {}
+for trend, var in trends.items():
+    percentage = var / total * 100
+    trend_percentages.update({trend: percentage})
+    print("PERCENTAGE OF %s : %s" % (trend, percentage))
+
+
+
